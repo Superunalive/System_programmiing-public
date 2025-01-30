9161fe122a8e8cf575c78b18d0292f5e41037bf6
Superunalive <161376015+Superunalive@users.noreply.github.com>
2025-01-30 17:38:51 +0000

    Lab 6 done

--- Lab_6/5.asm
+++ Lab_6/5.asm
@@ -1,5 +1,7 @@
 format elf64
 public _start
+public paint
+public change
 
 extrn initscr
 extrn endwin
@@ -13,12 +15,17 @@ extrn init_pair
 extrn addch
 extrn getch
 extrn timeout
+extrn mydelay
+extrn noecho
 include 'func.asm'
 
 maxx dq 0
-maxy dq 0
-palette dq 0
-count dq 0
+maxy dq 0'
+minx dq 0
+miny dq 0
+color dq 0
+loops dq 0
+speed dq 1000
 
 section ".text" executable
 
@@ -31,11 +38,11 @@ _start:
   call getmaxy
   mov [maxy], rax
   
-
+  call noecho
   call start_color
 
   ;this is blue - need red instead
-  mov rdx, 0x4
+  mov rdx, 0x1
   mov rsi, 0x0
   mov rdi, 0x1
   call init_pair
@@ -54,113 +61,234 @@ _start:
   call move
   call refresh
 
-  ;cycle for 1 side
+  ;4 loops in 1 for all sides of a square spiral
   xor rdi, rdi
+  xor rsi, rsi
   .dloop:
-    ;next symbol
-    xor rax, rax
-    mov rax, ' '
-    or rax, 0x100
-    mov rdi, rax
-    call addch
-
+    push rdi
+    push rsi
+    call paint
+    mov rdi, [speed]
+    call mydelay
     call refresh
+    call timeout
+    call getch
+    cmp rax, 'l'
+    je .exit
+    cmp rax, 'p'
+    jne @f
+      cmp [speed], 1000
+      jne .spd1
+      mov [speed], 10
+      jmp @f
+      .spd1:
+      mov [speed], 1000
+    @@:
 
-    ;moving down
-    xor rdi, rdi
-    xor rsi, rsi
-    mov rdi, [count]
+
+    pop rsi
+    pop rdi
     inc rdi
-    mov [count], rdi
+    push rsi
+    push rdi
     call move
     
     call refresh
+    pop rdi
+    pop rsi
+    cmp rdi, [maxy]
+    jne .dloop
 
-    mov rax, [count]
-    cmp rax, [maxy]
-    jb .dloop
-  
-  xor rdi, rdi
-  mov [count], 0
+  cmp [maxx], rsi
+  jbe .ending
   .rloop:
-    ;next symbol
-    xor rax, rax
-    mov rax, ' '
-    or rax, 0x100
-    mov rdi, rax
-    call addch
-
+    push rdi
+    push rsi
+    call paint
+    mov rdi, [speed]
+    call mydelay
     call refresh
+    call timeout
+    call getch
+    cmp rax, 'l'
+    je .exit
+    cmp rax, 'p'
+    jne @f
+      cmp [speed], 1000
+      jne .spd2
+      mov [speed], 10
+      jmp @f
+      .spd2:
+      mov [speed], 1000
+    @@:
 
-    ;moving right
-    mov rdi, [maxy]
-    xor rsi, rsi
-    mov rsi, [count]
+    pop rsi
+    pop rdi
     inc rsi
-    mov [count], rsi
+    push rdi
+    push rsi
     call move
-    
+
     call refresh
+    pop rsi
+    pop rdi
+    cmp rsi, [maxx]
+    jne .rloop
 
-    mov rax, [count]
-    cmp rax, [maxx]
-    jb .rloop
-
-  mov rdi, [maxy]
-  mov [count], rdi
+  cmp rdi, [miny]
+  jbe .ending
   .uloop:
-    ;moving up
-    xor rdi, rdi
-    mov rsi, [maxx]
-    mov rdi, [count]
+    push rdi
+    push rsi
+    call paint
+    mov rdi, [speed]
+    call mydelay
+    call refresh
+    call timeout
+    call getch
+    cmp rax, 'l'
+    je .exit
+    cmp rax, 'p'
+    jne @f
+      cmp [speed], 1000
+      jne .spd3
+      mov [speed], 10
+      jmp @f
+      .spd3:
+      mov [speed], 1000
+    @@:
+
+    pop rsi
+    pop rdi
     dec rdi
-    mov [count], rdi
+    push rdi
+    push rsi
     call move
-    
+
     call refresh
+    pop rsi
+    pop rdi
+    cmp rdi, [miny]
+    jne .uloop
 
-    ;next symbol
-    xor rax, rax
-    mov rax, ' '
-    or rax, 0x100
-    mov rdi, rax
-    call addch
+  mov rax, [minx]
+  inc rax
+  inc rax
+  mov [minx], rax
 
-    ;call getch
-    call refresh
-
-    mov rax, [count]
-    cmp rax, 0
-    ja .uloop
-
-  mov rdi, [maxx]
-  mov [count], rdi
+  cmp rsi, [minx]
+  jbe .ending
+  mov rax, [maxy]
+  dec rax
+  cmp rax, [miny]
+  je .ending
   .lloop:
-    ;moving left
-    xor rsi, rsi
-    xor rdi, rdi
-    mov rsi, [count]
+    push rdi
+    push rsi
+    call paint
+    mov rdi, [speed]
+    call mydelay
+    call refresh
+    mov rdi, 1
+    call timeout
+    call getch
+    cmp rax, 'l'
+    je .exit
+    cmp rax, 'p'
+    jne @f
+      cmp [speed], 1000
+      jne .spd4
+      mov [speed], 10
+      jmp @f
+      .spd4:
+      mov [speed], 1000
+    @@:
+
+    pop rsi
+    pop rdi
     dec rsi
-    mov [count], rsi
+    push rdi
+    push rsi
     call move
-    
+
     call refresh
+    pop rsi
+    pop rdi
+    cmp rsi, [minx]
+    jne .lloop
+  
+  mov rax, [maxy]
+  dec rax
+  dec rax
+  mov [maxy], rax
+  mov rax, [maxx]
+  dec rax
+  dec rax
+  mov [maxx], rax
+  mov rax, [miny]
+  inc rax
+  inc rax
+  mov [miny], rax
 
-    ;next symbol
-    xor rax, rax
-    mov rax, ' '
-    or rax, 0x100
-    mov rdi, rax
-    call addch
+  mov rax, [loops]
+  inc rax
+  mov [loops], rax
+  cmp rax, 1
+  jne @f
+  mov rax, [maxy]
+  dec rax
+  mov [maxy], rax
 
-    ;call getch
-    call refresh
+  @@:
+  mov rbx, [maxy]
+  dec rbx
+  cmp rdi, rbx
+  jb .dloop
+  
+  .ending:
+  call paint
+  call refresh
 
-    mov rax, [count]
-    cmp rax, 0
-    ja .lloop
+  xor rdi, rdi
+  xor rsi, rsi
+  call move
+  mov [loops], 0
+  mov [minx], 0
+  mov [miny], 0
 
-  call getch
-  call endwin
-  call exit
+  mov rdi, [stdscr]
+  call getmaxx
+  mov [maxx], rax
+  call getmaxy
+  mov [maxy], rax
+  xor rdi, rdi
+  xor rsi, rsi
+  
+  cmp [color], 0
+  jne @f
+  mov [color], 1
+  jmp .dloop
+  @@:
+  mov [color], 0
+  jmp .dloop
+  
+  .exit:
+    call endwin
+    call exit
 
+paint:
+  xor rax, rax
+  mov rax, ' '
+  cmp [color], 0
+  jne @f
+  or rax, 0x100
+  jmp .painted
+  @@:
+  or rax, 0x200
+  .painted:
+  mov rdi, rax
+  call addch
+  ret
+
+change:
+  ret
